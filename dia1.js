092178c6de23c372e30b459d62c080930c4e8d7f
Antonio de la Torre <antonio.delatorre@kaleidos.net>
2014-10-10 14:19:48 +0200

    day 1

---
+++ dia1.js
@@ -0,0 +1,43 @@
+DIA 1
+
+
+primitives
+
+objects
+
+
+typeOf null // objects
+
+
+Falsy
+
+undefined, null, false, '', 0, NaN
+
+Boolean(0) // false
+Boolean('hola') //true
+
+
+Igualdad
+
+=== !==
+
+== !==
+
+
+'3' == 3
+'3' === 3
+
+
+Functions
+
+
+Scope
+
+
+DUDA:   ??
+
+
+lazy evaluation
+
+
+
